262c847c78d4158ec3b711b2ecb32b46a3334afe
StefanBjornander <stefan.bjornander@outlook.com>
2020-12-12 22:43:52 +0100

    Backup

---
+++ C_Compiler_CSharp/C_Compiler_CSharp/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace CCompiler {
+  public class StaticBase {
+    private string m_uniqueName;
+    private int m_offset;
+  
+    public StaticBase(string name, int offset) {
+      m_uniqueName = name;
+      m_offset = offset;
+    }
+  
+    public string UniqueName {
+      get { return m_uniqueName; }
+    }
+  
+    public int Offset {
+      get { return m_offset; }
+    }
+
+    public override string ToString() {
+      if (m_offset > 0) {
+        return m_uniqueName + " + " + m_offset;
+      }
+      else if (m_offset < 0) {
+        return m_uniqueName + " - " + (-m_offset);
+      }
+      else {
+        return m_uniqueName;
+      }
+    }
+  }
+}
---
+++ C_Compiler_CSharp/C_Compiler_CSharp/StaticBase.cs
@@ -0,0 +1,45 @@
+﻿namespace CCompiler {
+  public abstract class StaticBase {    
+    private string m_uniqueName;
+    private int m_offset;
+  
+    public StaticBase(string name, int offset) {
+      m_uniqueName = name;
+      m_offset = offset;
+    }
+  
+    public string UniqueName {
+      get { return m_uniqueName; }
+    }
+  
+    public int Offset {
+      get { return m_offset; }
+    }
+
+    public override string ToString() {
+      if (m_offset > 0) {
+        return m_uniqueName + " + " + m_offset;
+      }
+      else if (m_offset < 0) {
+        return m_uniqueName + " - " + (-m_offset);
+      }
+      else {
+        return m_uniqueName;
+      }
+    }
+  }
+
+  public class StaticValue : StaticBase {
+    public StaticValue(string name, int offset)
+     :base(name, offset) {
+      // Empty.
+    }
+  }
+
+  public class StaticAddress : StaticBase {
+    public StaticAddress(string name, int offset)
+     :base(name, offset) {
+      // Empty.
+    }
+  }
+}
